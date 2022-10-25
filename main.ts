451ca808512a3d2a15a5dec7ec5b93aecff6f9bf
Linus Rundberg Streuli <schizoakustik@schizoakustik.se>
2022-10-25 11:59:11 +0200

    refactor

--- main.ts
+++ main.ts
@@ -1,14 +1,12 @@
+radio.onReceivedValue(function (name, value) {
+    if (name == "dir") {
+        servos.P2.setAngle(Math.map(value, 0, 1023, 0, 180))
+    } else {
+        pins.analogWritePin(AnalogPin.P4, value)
+    }
+})
 radio.setGroup(1)
-let speed = 0
 basic.forever(function () {
-    if (pins.analogReadPin(AnalogPin.P0) < 512) {
-        radio.sendNumber(0)
-    } else if (pins.analogReadPin(AnalogPin.P0) > 512) {
-        radio.sendNumber(1)
-    }
-    if (pins.analogReadPin(AnalogPin.P1) < 512) {
-        radio.sendNumber(2)
-    } else if (pins.analogReadPin(AnalogPin.P1) > 512) {
-        radio.sendNumber(3)
-    }
+    radio.sendValue("dir", pins.analogReadPin(AnalogPin.P1))
+    radio.sendValue("speed", pins.analogReadPin(AnalogPin.P0))
 })
